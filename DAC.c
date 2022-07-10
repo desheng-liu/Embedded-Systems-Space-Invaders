bfacd16b65c1f696ab6ef4360ce0c1506867eda6
Desheng Liu <65935640+desheng-liu@users.noreply.github.com>
2022-07-10 00:25:34 -0400

    Add files via upload

---
+++ DAC.c
@@ -0,0 +1,37 @@
+// dac.c
+// This software configures DAC output
+// Lab 6 requires a minimum of 4 bits for the DAC, but you could have 5 or 6 bits
+// Runs on LM4F120 or TM4C123
+// Program written by: put your names here
+// Date Created: 3/6/17 
+// Last Modified: 11/15/2021 
+// Lab number: 6
+// Hardware connections
+// TO STUDENTS "REMOVE THIS LINE AND SPECIFY YOUR HARDWARE********
+
+#include <stdint.h>
+#include "../inc/tm4c123gh6pm.h"
+// Code files contain the actual implemenation for public functions
+// this file also contains an private functions and private data
+
+// **************DAC_Init*********************
+// Initialize 6-bit DAC, called once 
+// Input: none
+// Output: none
+void DAC_Init(void){
+  volatile uint32_t delay; //create variable to use for delay
+	SYSCTL_RCGCGPIO_R |= 0x02; // turns on portB
+	delay = SYSCTL_RCGCGPIO_R; //waits for clock to stabilize
+	GPIO_PORTB_DEN_R |= 0x3F; //turns on PB 0-5
+	GPIO_PORTB_DIR_R |=0x03F; //sets PB 0-5 as OUTPUTS
+	GPIO_PORTB_DR8R_R |= 0x3F; //turns on 8mA drive on portB
+}
+
+// **************DAC_Out*********************
+// output to DAC
+// Input: 6-bit data, 0 to 63 
+// Input=n is converted to n*3.3V/63
+// Output: none
+void DAC_Out(uint32_t data){
+	GPIO_PORTB_DATA_R = data;
+}
